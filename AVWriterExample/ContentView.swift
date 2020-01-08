04010a2a57b82717bf4dc8fae51a1a72abf1a877
Ken <ken@torimaru.com>
2020-01-08 09:09:07 -0800

    Initial Commit

---
+++ AVWriterExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AVWriterExample
+//
+//  Created by Ken Torimaru on 1/8/20.
+//  Copyright © 2020 Torimaru & Williamson, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
